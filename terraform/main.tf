1d7b4938a996160b8a1acf2e5854eedc67023849
robpitcher <13648061+robpitcher@users.noreply.github.com>
2023-12-13 09:54:03 -0500

    fix: lint error

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,6 +2,3 @@ resource "azurerm_resource_group" "rust" {
   name     = var.projectName
   location = var.location
 }
-
-# For keyvault to provide tenant id
-data "azurerm_client_config" "current" {}

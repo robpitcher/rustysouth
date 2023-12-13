60070effde2e64be1010f7786a04fc6e4caeab01
robpitcher <13648061+robpitcher@users.noreply.github.com>
2023-12-13 17:49:13 -0500

    refactor: i dont care about resource lock

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,3 @@ resource "azurerm_resource_group" "rust" {
   name     = var.projectName
   location = var.location
 }
-
-resource "azurerm_management_lock" "rust_rg" {
-  name       = "terraform-applied-rg-level"
-  scope      = azurerm_resource_group.rust.id
-  lock_level = "CanNotDelete"
-  notes      = "This Resource Group had delete protection"
-}

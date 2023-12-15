b2259e0cf5f5473896c44f107a0a5c422230afb8
robpitcher <13648061+robpitcher@users.noreply.github.com>
2023-12-14 21:02:13 -0500

    fix linter

--- terraform/maintenance-config.tf
+++ terraform/maintenance-config.tf
@@ -7,9 +7,9 @@ resource "azurerm_maintenance_configuration" "rust" {
 
   window {
     start_date_time = "2023-12-15 05:00"
-    time_zone = "Eastern Standard Time"
-    duration = "03:55"
-    recur_every = "1Day"
+    time_zone       = "Eastern Standard Time"
+    duration        = "03:55"
+    recur_every     = "1Day"
   }
 
   in_guest_user_patch_mode = "User"
